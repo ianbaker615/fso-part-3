ceceeb3c3a84f3863c2ced9901ddbdbd83036b4e
Ian Baker <ianbaker615@gmail.com>
2021-03-30 14:10:28 -0400

    Exercise 3.2 phonebook backend part2

--- index.js
+++ index.js
@@ -26,6 +26,17 @@ let persons = [
   },
 ];
 
+// info page
+app.get("/info", (req, res) => {
+  const date = new Date();
+  res.send(
+    `<div>
+      <p>Phonebook has info for ${persons.length} people</p>
+      <p>${date}</p>
+    </div>`
+  );
+});
+
 // get all persons
 app.get("/api/persons", (req, res) => {
   res.json(persons);
