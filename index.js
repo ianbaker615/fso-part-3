637d606cf248476bd00252494c1b6426578e04eb
Ian Baker <ianbaker615@gmail.com>
2021-03-30 14:06:27 -0400

    Exercise 3.1 phonebook backend part1 initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let persons = [
+  {
+    name: "Arto Hellas",
+    phone: "040-123456",
+    id: 1,
+  },
+  {
+    name: "Ada Lovelace",
+    phone: "39-44-5323523",
+    id: 2,
+  },
+  {
+    name: "Dan Abramov",
+    phone: "12-43-234345",
+    id: 3,
+  },
+  {
+    name: "Mary Poppendieck",
+    phone: "39-23-6423122",
+    id: 4,
+  },
+];
+
+// get all persons
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
