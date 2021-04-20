a3019a4ab1060febdd0ac57392d6a1431662437f
Ian Baker <ianbaker615@gmail.com>
2021-04-20 14:29:22 -0400

    Exercises 3.15-3.18 phonebook connecting to mongodb backend

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const cors = require("cors");
 const Contact = require("./models/contact");
 
 app.use(cors());
-app.use(express.json());
 app.use(express.static("build"));
+app.use(express.json());
 morgan.token("body", (req, res) => JSON.stringify(req.body));
 app.use(
   morgan((tokens, req, res) => {
@@ -24,6 +24,29 @@ app.use(
     ].join(" ");
   })
 );
+
+//
+// CREATE
+//
+
+// add a contact
+app.post("/api/contacts", (req, res) => {
+  const body = req.body;
+  if (!body.name || !body.phone) {
+    return res.status(400).json({ error: "content missing" });
+  }
+  const contact = new Contact({
+    name: body.name,
+    phone: body.phone,
+  });
+  contact.save().then((savedContact) => {
+    res.json(savedContact);
+  });
+});
+
+//
+// READ
+//
 
 // info page
 app.get("/info", (req, res) => {
@@ -49,32 +72,72 @@ app.get("/api/contacts", (req, res) => {
 
 // get specific contact according to id
 app.get("/api/contacts/:id", (req, res) => {
-  Contact.findById(req.params.id).then((contact) => {
-    res.json(contact);
-  });
+  Contact.findById(req.params.id)
+    .then((contact) => {
+      if (contact) {
+        res.json(contact);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-// add a contact
-app.post("/api/contacts", (req, res) => {
-  const body = req.body;
-  if (!body.name || !body.phone) {
-    return res.status(400).json({ error: "content missing" });
-  }
-  const contact = new Contact({
-    name: body.name,
-    phone: body.phone,
-  });
-  contact.save().then((savedContact) => {
-    res.json(savedContact);
-  });
+//
+// UPDATE
+//
+
+// update a contact
+app.put("/api/contacts/:id", (req, res, next) => {
+  const updatedContact = { name: req.body.name, phone: req.body.phone };
+  Contact.findByIdAndUpdate(req.params.id, updatedContact)
+    .then((contact) => {
+      res.json(updatedContact);
+      res.status(200).end();
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
+
+//
+// DELETE
+//
 
 // delete a contact
-app.delete("/api/contacts/:id", (req, res) => {
-  const id = Number(req.params.id);
-  contacts = contacts.filter((contact) => contact.id !== id);
-  res.status(204).end();
+app.delete("/api/contacts/:id", (req, res, next) => {
+  Contact.findByIdAndRemove(req.params.id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((error) => next(error));
 });
+
+//
+// ERROR HANDLING
+//
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: "unknown endpoint" });
+};
+// handler of requests w unknown endpoint
+app.use(unknownEndpoint);
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return res.status(404).send({ error: "incorrectly formatted id" });
+  }
+
+  next(error);
+};
+// handler of requests with errors as result
+app.use(errorHandler);
+
+//
+// SERVER SETUP
+//
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
